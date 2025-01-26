f18c773b8bedea0406cabec5cc2a48118f2b7460
slembcke <slembcke@gmail.com>
2025-01-26 13:56:01 -0600

    doors!

--- src/data.s
+++ src/data.s
@@ -15,14 +15,16 @@ inclz4 _BOBY, "chr/Boby.lz4"
 	symbol: .incbin file
 .endmacro
 
+.segment "PRG1"
 inclevel _MAP_SPLASH, "map/splash.bin"
 inclevel _MAP_LEVEL1, "map/Level1.bin"
 inclevel _MAP_LEVEL2, "map/Level2.bin"
 inclevel _MAP_LEVEL3, "map/Level3.bin"
 inclevel _MAP_LEVEL4, "map/Level4.bin"
 
-; inclevel _MAP_LEVEL5, "map/Level5.bin"
-; inclevel _MAP_LEVEL6, "map/Level6.bin"
-; inclevel _MAP_LEVEL7, "map/Level7.bin"
-; inclevel _MAP_LEVEL8, "map/Level8.bin"
-; inclevel _MAP_LEVEL9, "map/Level9.bin"
+.segment "PRG2"
+inclevel _MAP_LEVEL5, "map/Level5.bin"
+inclevel _MAP_LEVEL6, "map/Level6.bin"
+inclevel _MAP_LEVEL7, "map/Level7.bin"
+inclevel _MAP_LEVEL8, "map/Level8.bin"
+inclevel _MAP_LEVEL9, "map/Level9.bin"
